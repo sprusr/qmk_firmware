6cd8a8aaec047eabb1b8189ead7f6ef6fea68efc
Scott Street <scott@spru.sr>
2022-04-11 13:54:39 +0300

    Configure moonlander/sprusr thumb cluster

--- keyboards/moonlander/keymaps/sprusr/keymap.c
+++ keyboards/moonlander/keymaps/sprusr/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,           KC_MEH,  KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), LGUI_T(KC_QUOT),
         KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), KC_RSFT,
     LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT, KC_RGHT,  LALT_T(KC_APP),    RCTL_T(KC_ESC),   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
-                                            KC_SPC,  KC_BSPC, KC_LGUI,           KC_LALT,  KC_TAB,  KC_ENT
+                                           MO(SYMB), KC_SPC,  KC_DEL,           KC_BSPC, KC_ENT,   MO(MDIA)
     ),
 
     [SYMB] = LAYOUT_moonlander(
